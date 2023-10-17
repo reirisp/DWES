79f4a9f4f0f29e79848d3772c01dfb623cadbae5
rodrigo <reirisp@fpcoruna.afundacion.org>
2023-10-17 14:46:57 +0200

    añadidos los apartados 2, 3, 4 para completar el main.php

--- sprint2sql/www/mysite/main.php
+++ sprint2sql/www/mysite/main.php
@@ -1,4 +1,33 @@
 <?php
 	$db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die ('Fail');
 ?>
-<h1>Conexión establecida</h1>
+<html>
+	<head>
+		<style>
+			img{
+				width: 350px;
+				height: 200px;
+			}
+		</style>
+	</head>
+	<body>
+		<h1>Conexión establecida</h1>
+		<?php
+			// Lanzar una query
+			$query = 'SELECT * FROM tJuegos';
+			$result = mysqli_query($db, $query) or die ('Query error');
+
+			// Recorrer el resultado
+			while ($row = mysqli_fetch_array($result)){
+				echo '<ul>';
+				echo '<h1><a href = "/detail.php?id='.$row[0].'">'.$row[0].'. '.$row[1].'</a></h1>';
+				echo '<img src ='.$row[2].'></img>';
+				echo '<li>Año: '.$row[3].'</li>';
+				echo '<li>Compañía: '.$row[4].'</li>';
+				echo '</ul>';
+				echo '<hr>';
+			}
+		mysqli_close($db);
+		?>
+	</body>
+</html>
