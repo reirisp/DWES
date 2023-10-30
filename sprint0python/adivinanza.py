9bd5414ec0f3d2a1da96e395f0c310433f70a40e
Rodrigo <145001101+reirisp@users.noreply.github.com>
2023-10-30 13:44:31 +0100

    Ejercicio 2.

--- sprint0python/adivinanza.py
+++ sprint0python/adivinanza.py
@@ -8,3 +8,23 @@ if __name__ == "__main__":
         print('Has acertado')
     else:
         print('No has acertado')
+
+    print('Adivina la adivinanza')
+    print('A. Otra adivinanza')
+    print('B. Adivinanza')
+    print('C. Adivinanza')
+    respuesta = input('Cual es la opción correcta: ')
+    if respuesta == 'A':
+        print('Has acertado')
+    else:
+        print('No has acertado')
+
+    print('Adivina la adivinanza')
+    print('A. Adivinanza')
+    print('B. Adivinanza')
+    print('C. Super Adivinanza')
+    respuesta = input('Cual es la opción correcta: ')
+    if respuesta == 'C':
+        print('Has acertado')
+    else:
+        print('No has acertado')
