6d53d2e64d7959017a372122832037ce4cfb1283
Rodrigo <145001101+reirisp@users.noreply.github.com>
2023-11-02 10:26:18 +0100

    Ejercicio 6, dos adivinanzas al azar

--- sprint0python/adivinanza.py
+++ sprint0python/adivinanza.py
@@ -1,65 +1,46 @@
+import random
+
+# Lista de adivinanzas
+adivinanzas = [
+    {
+        "pregunta": "Qué tiene llaves pero no puede abrir puertas?",
+        "opciones": ["a) Un pájaro", "b) Un piano", "c) Un teléfono"],
+        "respuesta": "b"
+    },
+    {
+        "pregunta": "Tiene hojas, pero no es un libro; se mueve, pero no es un animal. ¿Qué es?",
+        "opciones": ["a) Un árbol", "b) Un coche", "c) Un zapato"],
+        "respuesta": "a"
+    },
+    {
+        "pregunta": "Es redondo como una luna, pero no es el satélite de la Tierra. ¿Qué es?",
+        "opciones": ["a) Una pelota", "b) Una rueda", "c) Una galleta"],
+        "respuesta": "b"
+    }
+]
+
 def adivinanza():
+    # Selecciona dos adivinanzas aleatorias sin repeticiones
+    adivinanza1, adivinanza2 = random.sample(adivinanzas, 2)
+
     puntuacion = 0
-    
-    # Primera adivinanza
-    # Con print imprimos mensajes por pantalla
-    print("Adivina la respuesta correcta:")
-    print("Qué tiene llaves pero no puede abrir puertas?")
-    print("a) Un pájaro")
-    print("b) Un piano")
-    print("c) Un teléfono")
 
-    # Para que el usuario pueda introducir su opción
-    respuesta = input("Tu respuesta (a/b/c): ").lower()
-    
-    # Creamos un if else para que si acierta muestre un mensaje y si falla muestre otro distinto
-    if respuesta == "b":
-        print("¡Correcto!")
-        puntuacion += 10
-    else:
-        print("Respuesta incorrecta. La respuesta correcta es 'b) Un piano'.")
-        puntuacion -= 5
-    
-    # Segunda adivinanza
-    # Con print imprimos mensajes por pantalla
-    print("\nAdivina la respuesta correcta:")
-    print("Tiene hojas, pero no es un libro; se mueve, pero no es un animal. ¿Qué es?")
-    print("a) Un árbol")
-    print("b) Un coche")
-    print("c) Un zapato")
-    
-    # Para que el usuario pueda introducir su opción
-    respuesta = input("Tu respuesta (a/b/c): ").lower()
-    
-    # Creamos un if else para que si acierta muestre un mensaje y si falla muestre otro distinto
-    if respuesta == "a":
-        print("¡Correcto!")
-        puntuacion += 10
-    else:
-        print("Respuesta incorrecta. La respuesta correcta es 'a) Un árbol'.")
-        puntuacion -= 5
-    
-    # Tercera adivinanza
-    # Con print imprimos mensajes por pantalla
-    print("\nAdivina la respuesta correcta:")
-    print("Es redondo como una luna, pero no es el satélite de la Tierra. ¿Qué es?")
-    print("a) Una pelota")
-    print("b) Una galleta")
-    print("c) Una rueda")
-    
-    # Para que el usuario pueda introducir su opción
-    respuesta = input("Tu respuesta (a/b/c): ").lower()
-    
-    # Creamos un if else para que si acierta muestre un mensaje y si falla muestre otro distinto
-    if respuesta == "c":
-        print("¡Correcto!")
-        puntuacion += 10
-    else:
-        print("Respuesta incorrecta. La respuesta correcta es 'c) Una rueda'.")
-        puntuacion -= 5
+    for bucleAdivinanzas in [adivinanza1, adivinanza2]:
+        print("\nAdivina la respuesta correcta:")
+        print(bucleAdivinanzas["pregunta"])
+        for opcion in bucleAdivinanzas["opciones"]:
+            print(opcion)
 
-    # Muestra la puntuación final
-    print("\nPuntuación final: ", puntuacion)
+        respuesta = input("Tu respuesta (a/b/c): ").lower()
+
+        if respuesta == bucleAdivinanzas["respuesta"]:
+            print("¡Correcto! La respuesta es correcta.")
+            puntuacion += 10
+        else:
+            print("Respuesta incorrecta. La respuesta correcta es:", bucleAdivinanzas["respuesta"])
+            puntuacion -= 5
+
+    print("\nPuntuación final:", puntuacion)
 
 if __name__ == "__main__":
     adivinanza()
