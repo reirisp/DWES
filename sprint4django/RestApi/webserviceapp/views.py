28fd640a861f389c6bc67f469b23fa2b721c8978
rodrigo <reirisp@fpcoruna.afundacion.org>
2023-11-21 14:02:18 +0100

    Tercer EndPoint

--- sprint4django/RestApi/webserviceapp/views.py
+++ sprint4django/RestApi/webserviceapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from .models import Tjuegos
+from django.views.decorators.csrf import csrf_exempt
+from .models import Tjuegos, Tcomentarios
+import json
 
 # Create your views here.
 
@@ -39,3 +41,16 @@ def devolver_juegos_por_id(request, id_solicitado):
 	}
 
 	return JsonResponse(resultado, json_dumps_params={'ensure_ascii': False})
+
+@csrf_exempt
+def guardar_comentario(request, juego_id):
+	if request.method != 'POST':
+		return None
+	
+	json_peticion = json.loads(request.body)
+	comentario = Tcomentarios()
+	comentario.comentario = json_peticion['nuevo_comentario']
+	comentario.juego = Tjuegos.objects.get(id = juego_id)
+	comentario.save()
+
+	return JsonResponse({"status": "ok"})
