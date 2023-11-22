288d1f8bf47b153fb99dfb56d2b7c09a6d5ce6a9
rodrigo <reirisp@fpcoruna.afundacion.org>
2023-11-22 14:04:30 +0100

    dejando todo bien subido al repositorio

--- sprint2sql/mysitedb.sql
+++ sprint2sql/mysitedb.sql
@@ -18,6 +18,8 @@
 --
 -- Table structure for table `tComentarios`
 --
+CREATE Database mysitedb;
+USE mysitedb;
 
 DROP TABLE IF EXISTS `tComentarios`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
