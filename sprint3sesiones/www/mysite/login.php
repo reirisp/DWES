d951bf660829e40146fb8857e08c8bd17811ab43
rodrigo <reirisp@fpcoruna.afundacion.org>
2023-11-22 14:17:38 +0100

    cambiando localhost por la IP

--- sprint3sesiones/www/mysite/login.php
+++ sprint3sesiones/www/mysite/login.php
@@ -1,6 +1,6 @@
 <?php
     // Conexión con la BBDD
-    $db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
+    $db = mysqli_connect('172.16.0.2', 'root', '1234', 'mysitedb') or die('Fail');
 ?>
 <html>
     <body>
